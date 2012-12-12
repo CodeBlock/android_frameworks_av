5a488085c528820108803e3eb0c1f093413d3db4
Ricky Elrod <ricky@elrod.me>
2012-12-12 04:50:09 -0500

    $(CEDARX_ANDROID_CODE) isn't getting defined.

--- media/CedarX-Projects/CedarAndroidLib/Android.mk
+++ media/CedarX-Projects/CedarAndroidLib/Android.mk
@@ -6,6 +6,7 @@ ifneq ($(CEDARX_DEBUG_CEDARV),Y)
 ifeq ($(CEDARX_ANDROID_VERSION),4)
 include $(LOCAL_PATH)/LIB_F23/Android.mk
 else
-include $(LOCAL_PATH)/LIB_$(CEDARX_ANDROID_CODE)_F23/Android.mk
+# TODO: Why is $(CEDARX_ANDROID_CODE) not getting defined?
+include $(LOCAL_PATH)/LIB_JB_F23/Android.mk
 endif
 endif
